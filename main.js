dcdffc2272a52292caa3c1a7f3897eaad6a65e9b
Tommaso <tommaso.macco@gmail.com>
2021-11-16 10:29:10 +0100

    data js vue done contacts

--- main.js
+++ main.js
@@ -1,6 +1,57 @@
 var app = new Vue({
     el: '#app',
     data: {
-      
+      contacts: [
+        { /* start contact */
+          name: "Michele",
+          avatar: "img/avatar_1.jpg",
+          visible: "true",
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              text: "Hai portato a spasso il cane?",
+              status: "sent",
+            }
+          ]
+        },
+        {
+          name: "Fabio",
+          avatar: "img/avatar_2.jpg",
+          visible: "true",
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              text: "Hai portato a spasso il cane?",
+              status: "sent",
+            }
+          ]
+        },
+        {
+          name: "Samuele",
+          avatar: "img/avatar_3.jpg",
+          visible: "true",
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              text: "Hai portato a spasso il cane?",
+              status: "sent",
+            }
+          ]
+        },
+        {
+          name: "Luisa",
+          avatar: "img/avatar_4.jpg",
+          visible: "true",
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              text: "Hai portato a spasso il cane?",
+              status: "sent",
+            }
+          ]
+        },
+      ]
     },
-  })
+  });
+
+  
